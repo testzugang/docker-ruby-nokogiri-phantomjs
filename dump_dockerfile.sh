271a14ea79fc6d5ee72d9b5c4c2b8ac364c90324
Yusuke Watase <ywatase@gmail.com>
2017-03-10 14:30:56 +0900

    install several nokogiri version: 'latest', '~> 1.7.0', '~> 1.6.0'

--- dump_dockerfile.sh
+++ dump_dockerfile.sh
@@ -9,47 +9,53 @@ ruby_versions="2.1 2.2 2.3 2.4 2 latest"
 additional_os="alpine"
 
 main() {
-[ -d Dockerfiles ] || mkdir Dockerfiles
+    [ -d Dockerfiles ] || mkdir Dockerfiles
 
-for os in "" $additional_os
-do
-  for ruby_ver in $ruby_versions
-  do
-    if [ "$os" = "" ] ; then
-      tag=$ruby_ver
-    elif [ "$ruby_ver" = "latest" ] ; then
-      tag=$os
-    else
-      tag=$ruby_ver-$os
-    fi
-	dump_dockerfile > Dockerfiles/Dockerfile.$tag
-  done
-done
+    for os in "" $additional_os
+    do
+        for ruby_ver in $ruby_versions
+        do
+            if [ "$os" = "" ] ; then
+                tag=$ruby_ver
+            elif [ "$ruby_ver" = "latest" ] ; then
+                tag=$os
+            else
+                tag=$ruby_ver-$os
+            fi
+            dump_dockerfile > Dockerfiles/Dockerfile.$tag
+        done
+    done
 }
 
 dump_dockerfile () {
-echo FROM ruby:$tag
-dump_package
-echo RUN gem install nokogiri
-dump_phantomjs
+    echo FROM ruby:$tag
+    dump_package
+    dump_nokogiri
+    dump_phantomjs
+}
+
+dump_nokogiri () {
+    echo RUN gem install nokogiri
+    echo "RUN gem install nokogiri -v '~> 1.7.0'"
+    echo "RUN gem install nokogiri -v '~> 1.6.0'"
 }
 
 dump_package () {
-	if [ "$os" = "alpine" ] ; then
-		# phantomjs: openssl curl
-		echo RUN apk add --no-cache build-base libxml2-dev libxslt-dev openssl curl
-	else
-		# phantomjs: libssl-dev libfontconfig1
-		cat <<'END'
+    if [ "$os" = "alpine" ] ; then
+        # phantomjs: openssl curl
+        echo RUN apk add --no-cache build-base libxml2-dev libxslt-dev openssl curl
+    else
+        # phantomjs: libssl-dev libfontconfig1
+        cat <<'END'
 RUN apt-get update \
     && apt-get install -y ruby-dev zlib1g-dev liblzma-dev build-essential git libfontconfig1 libssl-dev \
     && rm -rf /var/lib/apt/lists/*
 END
-	fi
+    fi
 }
 
 dump_phantomjs() {
-  cat <<END
+    cat <<END
 RUN curl -o /tmp/${pjs_file} -L https://github.com/Medium/phantomjs/releases/download/v${pjs_ver}/${pjs_file} \\
     && md5sum /tmp/${pjs_file} | grep -q "$pjs_hash" \\
     && tar -xjf /tmp/${pjs_file} -C /tmp \\
@@ -60,3 +66,5 @@ END
 }
 
 main "$@"
+
+# vim: set et ts=4 sw=4 sts=4:
